445fc79ad8eab1d5a4281a0a95932db0f1beec2b
nederjair <nederjair@gmail.com>
2026-01-08 16:32:48 +0300

    added encoding

--- pm.py
+++ pm.py
@@ -2,9 +2,9 @@ import numpy as np
 from random import randint
 
 
-def encode_col(func_count, arg_count, use_just_states_array):
+def encode_col(func_count, arg_count, row_count):
     # arg_count is the length of the argument array
-    col = np.zeros(4, dtype=np.float64)
+    col = np.zeros(row_count, dtype=int)
     # function of two arguments
     col[0] = randint(0, func_count)
     # first argument
@@ -13,20 +13,30 @@ def encode_col(func_count, arg_count, use_just_states_array):
     col[1] = randint(1, arg_count)
     # second argument
     col[2] = randint(1, arg_count)
-    # which argument array to use
-    # 0-the states and qs array
-    # 1-the subexpression array
-    if use_just_states_array:
-        col[3] = 0
-    else:
-        col[3] = randint(0, 1)
     return col
 
 
-def encode_array(func_count, arg_count, subexpr_count, col_count, recursion_level):
+def encode_array(func_count, row_count, col_count, recursion_level):
     states_array_count = 3 * 2 * (recursion_level + 1)
-    subexpr_array_count = 0
-    for i in range(col_count):
-        col = encode_col(func_count, arg_count, subexpr_count)
+    pm_array = np.zeros((row_count, col_count), dtype=int)
 
-    pass
+    # encode the first array where just the array of states is used
+    arg_count = states_array_count
+    pm_array[0] = encode_col(func_count, arg_count, row_count)
+    # force to use the array of states
+    # because the array of subexpr is empty at the beginning
+    pm_array[0, 3] = 0
+    # now the subexpression array is not empty (has one element)
+    subexpr_array_count = 1
+    for i in range(1, col_count):
+        # which argument array to use
+        # 0-the states and qs array
+        # 1-the subexpression array
+        pm_array[i, 3] = randint(0, 1)
+        if pm_array[i, 3] == 0:  # array of states
+            arg_count = states_array_count
+            pm_array[i, 0:2] = encode_col(func_count, arg_count, row_count)
+        else:
+            arg_count = subexpr_array_count
+            pm_array[i, 0:2] = encode_col(func_count, arg_count, row_count)
+        subexpr_array_count += 1
